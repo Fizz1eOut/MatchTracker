5fa079f634a8be724f3eab21473144d8e0493e4d
FizzleOut <fizzleout1337@gmail.com>
2025-02-11 19:01:33 +0100

    feat: Add container component with size support via Vue 3 Composition API and TypeScript - Defined component properties, including size with possible values: 'xs', 'sm', 'md', 'lg', 'xl'. - Added computed property `container` to dynamically assign class depending on size. - Created component template using slots. - Added styles for different container sizes, including responsive styles for different screen widths.

---
+++ src/components/Base/AppContainer.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+  import { computed } from 'vue';
+
+  // Определяем пропсы с дефолтным значением через withDefaults
+  const props = withDefaults(
+    defineProps<{
+      size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+    }>(),
+    {
+      size: 'xs', // Дефолтное значение
+    }
+  );
+
+  // Создаем вычисляемое свойство
+  const container = computed(() => `size-${props.size}`);
+</script>
+
+<template>
+  <div :class="container" class="container">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+  .container {
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 0 15px;
+  }
+
+  .size-xl {
+    padding: 50px;
+  }
+
+  .size-lg {
+    padding: 40px;
+  }
+
+  .size-md {
+    padding: 30px;
+  }
+
+  .size-sm {
+    padding: 20px;
+  }
+
+  .size-xs {
+    padding: 16px;
+  }
+
+  @media (max-width: 768px) {
+    .size-xl {
+      padding: 30px;
+    }
+  }
+
+  @media (max-width: 499px) {
+    .size-md {
+      padding: 16px;
+    }
+    .size-xl {
+      padding: 20px;
+    }
+  }
+</style>
