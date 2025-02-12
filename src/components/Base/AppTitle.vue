d48734652caa368e4c38b47956c2e4c56dafb234
FizzleOut <fizzleout1337@gmail.com>
2025-02-12 18:27:25 +0100

    feat: add new AppTitle, AppSubtitle and Underlay components - Create `AppTitle` and `AppSubtitle` components: - Implement slots for dynamic content. - Add styles for titles and subtitles, including responsive styles.
    - Create an `Underlay` component to wrap elements:
    - Implement slots for dynamic content.
    - Add styles for the component, including rounded corners and background color.

---
+++ src/components/Base/AppTitle.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  
+</script>
+<template>
+  <h2 class="title">
+    <slot></slot>
+  </h2>
+</template>
+<style scoped>
+.title {
+  font-family: "Roboto Condensed", serif;
+  font-size: 32px;
+  font-weight: 600;
+  color: var(--color-white);
+  }  
+@media (max-width: 499.98px) {
+  .title {
+    font-size: 24px;
+    font-weight: 600;
+  }  
+}
+</style>
---
+++ src/components/Base/AppUnderlay.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  
+</script>
+<template>
+  <div class="underlay">
+    <slot />
+  </div>
+</template>
+<style scoped>
+  .underlay {
+    border-radius: 10px;
+    background-color: var(--color-underlay);
+  }
+</style>
