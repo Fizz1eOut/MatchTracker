6556ab59f50d52513fcaee99ad856bb43d550b89
FizzleOut <fizzleout1337@gmail.com>
2025-03-23 18:08:11 +0100

    Fix: added processing of match request without specifying leagueId
    - Changed the request for getting matches for the case when leagueId is not passed
    - If leagueId is not passed, a universal endpoint for matches is used
    - Updated the logic for generating URLs for the request

--- src/api/matches.ts
+++ src/api/matches.ts
@@ -1,10 +1,16 @@
 import { fetchData } from '@/modules/http';
 import type { MatchesResponse, Match } from '@/interface/matches.interface';
 
-const API_URL = 'https://vue-football-proxy.onrender.com/api/competitions';
+const API_URL = 'https://vue-football-proxy.onrender.com/api';
 
 export const getMatches = async (leagueId?: string): Promise<Match[]> => {
-  const url = `${API_URL}/${leagueId}/matches`;
+  let url = '';
+
+  if (leagueId) {
+    url = `${API_URL}/competitions/${leagueId}/matches`;
+  } else {
+    url = `${API_URL}/matches`;
+  }
 
   const response = await fetchData<MatchesResponse>(url);
 
