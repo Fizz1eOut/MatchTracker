e63170d1ffd0fe6cdf37289bce21313083229147
FizzleOut <fizzleout1337@gmail.com>
2025-04-12 10:39:50 +0200

    refactor(AppNav, AppNavMobile): updated navigation, reworked structure and added icons
    - Updated links in AppNav: removed Home, Standings, Scorers and added Matches, Competitions, News.
    - New navigation structure implemented in AppNavMobile using AppUnderlay and AppIcon.
    - Added icons to menu items in mobile navigation.
    - Removed AppLeaguesDropdown and menu close events, simplified logic.
    - Updated style and behavior of mobile menu.

--- src/components/Base/AppNav.vue
+++ src/components/Base/AppNav.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
   import AppLink from '@/components/Base/AppLink.vue';
-  import AppLeaguesDropdown from '@/components/Base/AppLeaguesDropdown.vue';
 </script>
 
 <template>
@@ -8,29 +7,19 @@
     <div class="nav__body">
       <div class="nav__item">
         <router-link to="/">
-          <app-link>Home</app-link>
-        </router-link>
-      </div>
-
-      <div class="nav__item">
-        <app-leagues-dropdown />
-      </div>
-
-      <div class="nav__item">
-        <router-link to="">
           <app-link>Matches</app-link>
         </router-link>
       </div>
 
       <div class="nav__item">
-        <router-link to="">
-          <app-link>Standings</app-link>
+        <router-link to="/competitions">
+          <app-link>Competitions</app-link>
         </router-link>
       </div>
 
       <div class="nav__item">
-        <router-link to="">
-          <app-link>Scorers</app-link>
+        <router-link to="/news">
+          <app-link>News</app-link>
         </router-link>
       </div>
     </div>
@@ -42,14 +31,12 @@
     max-width: 550px;
     width: 100%;
   }
-
   .nav__body {
     display: flex;
     align-items: center;
     justify-content: space-between;
     gap: 10px;
   }
-
   .router-link-active .link {
     cursor: pointer;
     color: var(--color-white);
--- src/components/Base/AppNavMobile.vue
+++ src/components/Base/AppNavMobile.vue
@@ -1,71 +1,60 @@
 <script setup lang="ts">
   import AppLink from '@/components/Base/AppLink.vue';
   import AppContainer from '@/components/Base/AppContainer.vue';
-  import AppLeaguesDropdown from '@/components/Base/AppLeaguesDropdown.vue';
-
-  interface NavMobileProps {
-    open: boolean;
-  }
-
-  defineProps<NavMobileProps>();
-
-  // Эмиссия событий
-  const emit = defineEmits<{
-    (e: 'close'): void;
-  }>();
-
-  const close = () => {
-    emit('close');
-  };
+  import AppUnderlay from '@/components/Base/AppUnderlay.vue';
+  import AppIcon from '@/components/Base/AppIcon.vue';
 </script>
 
 <template>
-  <Transition>
-    <nav
-      v-if="open" 
-      class="nav-modile" 
-    >
+  <nav class="nav-modile">
+    <app-underlay class="nav-mobile__underlay">
       <app-container>
         <div class="nav-mobile__body">
           <div class="nav-mobile__item">
-            <router-link to="/" @click="close">
+            <router-link to="/">
               <app-link class="nav-mobile__link">
-                Home
+                <app-icon 
+                  name="ball" 
+                  size="20" 
+                  color="var(--color-gray)" 
+                  class="nav-mobile__icon"
+                />
+                Matches
               </app-link>
             </router-link>
           </div>
 
           <div class="nav-mobile__item">
-            <app-leagues-dropdown />
-          </div>
-
-          <div class="nav-mobile__item">
-            <router-link to="" @click="close">
+            <router-link to="/competitions">
               <app-link class="nav-mobile__link">
-                Matches
+                <app-icon 
+                  name="cup" 
+                  size="20" 
+                  color="var(--color-gray)"
+                  class="nav-mobile__icon"
+                />
+                Competitions
               </app-link>
             </router-link>
           </div>
           
           <div class="nav-mobile__item">
-            <router-link to="" @click="close">
+            <router-link to="/news">
               <app-link class="nav-mobile__link">
-                Standings
-              </app-link>
-            </router-link>
-          </div>
-
-          <div class="nav-mobile__item">
-            <router-link to="" @click="close">
-              <app-link class="nav-mobile__link">
-                Scorers
+                <app-icon 
+                  name="newspaper" 
+                  size="20" 
+                  color="var(--color-gray)"
+                  class="nav-mobile__icon"
+                />
+                News
               </app-link>
             </router-link>
           </div>
         </div>
       </app-container>
-    </nav>
-  </Transition>
+    </app-underlay>
+  </nav>
 </template>
 
 <style scoped>
@@ -73,39 +62,36 @@
     cursor: pointer;
     color: var(--color-white);
   }
-
-  @media (max-width: 768px) {
-    .nav-modile {
-      position: fixed;
-      left: 0;
-      top: 0px;
-      max-width: 350px;
-      width: 100%;
-      height: 100vh;
-      z-index: 9;
-      box-shadow: 0px 0px 10px 1px rgb(0 0 0 / 25%);
-      background-color: var(--color-underlay);
-    }
-    .nav-mobile__body {
-      margin-top: 100px;
-    }
-    .nav-mobile__body >*:not(:last-child) {
-      margin-bottom: 20px;
-    }
-
-    .v-enter-active,
-    .v-leave-active {
-      transition: opacity 0.9s ease-in-out;
-    }
-
-    .v-enter-from,
-    .v-leave-to {
-      opacity: 0;
-    }
+  .router-link-active .nav-mobile__icon {
+    cursor: pointer;
+    color: var(--color-white);
   }
-  @media (max-width: 499px) {
-    .nav-modile {
-      max-width: 100%;
-    }
+  .nav-mobile__icon {
+    margin-bottom: 5px;
+  }
+  .nav-modile {
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    z-index: 10;
+  }
+  .nav-mobile__underlay {
+    border-radius: 0;
+  }
+  .nav-mobile__body {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    gap: 10px;
+    width: 100%;
+    text-align: center;
+  }
+  .nav-mobile__link {
+    font-size: 13px;
+    font-weight: 400;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
 </style>
