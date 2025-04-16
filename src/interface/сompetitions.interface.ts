3e71e6760e9741f75c556446c14f4dbd40239400
FizzleOut <fizzleout1337@gmail.com>
2025-04-16 19:27:17 +0200

    types: competitions interface expanded with Area and Season
    - New interfaces added: Area and Season
    - Competition interface expanded: currentSeason and area fields added
    - CompetitionsResponse type updated with new data

--- src/interface/сompetitions.interface.ts
+++ src/interface/сompetitions.interface.ts
@@ -1,9 +1,26 @@
+export interface Area {
+  id: number;
+  name: string;
+  code: string;
+  flag: string;
+  winner?: string | null;
+}
+
+export interface Season {
+  id: number;
+  startDate: string;
+  endDate: string;
+  currentMatchday: number;
+}
+
 export interface Competition {
   id: number;
   name: string;
   code: string;
   type: string;
   emblem: string;
+  currentSeason: Season;
+  area: Area;
 }
 
 export interface CompetitionsResponse {
